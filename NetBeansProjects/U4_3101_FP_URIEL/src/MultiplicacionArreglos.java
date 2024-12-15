b634789150b0d0f01b257b596606510a52d273e7
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-14 22:17:27 -0600

    Ejercicio de arreglos

--- NetBeansProjects/U4_3101_FP_URIEL/src/MultiplicacionArreglos.java
+++ NetBeansProjects/U4_3101_FP_URIEL/src/MultiplicacionArreglos.java
@@ -2,8 +2,6 @@
 import javax.swing.JOptionPane;
 public class MultiplicacionArreglos {
     public static void main(String[] args) {
-        String sup[] = {"0","1","2"};
-        //String izq[] = {"0 |","1 |","2 |"};
         byte tabla1[][] = new byte [3][3];
         byte tabla2[][] = new byte [3][3];
         short tabla3[][] = new short [3][3];
@@ -25,12 +23,6 @@ public class MultiplicacionArreglos {
             for(b = 0; b < 3; b++){
                 tabla3[a][b] = (short) (tabla1[a][b] * tabla2 [z-a][z-b]);    
             }    
-        }
-        for (a = 0; a < 3; a++){
-            for (b = 0; b < 3; b++){
-                System.out.print(sup[b] + "|\t");
-            }
-            System.out.print("   \t");
         }    
         System.out.println();
         for (a = 0; a < 3; a++){    
